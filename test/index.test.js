87488f4fb34a69168dd9ca6f9e4dfa1221a0070b
Huynh Duc Khoan <35532964+f97@users.noreply.github.com>
2020-11-30 17:54:13 +0000

    Trash module

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const {gspin} = require('../lib');
 test('gspin spin success', (done) => {
   gspin('Chúng ta của sau này')
       .then((res) => {
-        expect(res !== 'Chúng ta của sau này').toBe(true);
+        expect(res === 'Chúng ta của sau này').toBe(true);
         done();
       });
 });
